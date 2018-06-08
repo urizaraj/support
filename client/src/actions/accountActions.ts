9f40ac7816b55e9e570ebf22f43988f8fa5db60a
Alexander Urizar <urizaraj@gmail.com>
2018-06-08 13:28:10 -0400

    Create signup action, user checkResp, and rename loginAlso in accountActions

--- client/src/actions/accountActions.ts
+++ client/src/actions/accountActions.ts
@@ -1,17 +1,8 @@
+import { checkResp } from 'functions'
 import { Dispatch } from 'react-redux'
 import { State } from 'reducers'
 
-export function login() {
-  return (dispatch: any, getState: any) => {
-    const state = getState() as State
-  }
-}
-
-export function loginAlso(
-  emailAddress: string,
-  password: string,
-  history: any
-) {
+export function login(emailAddress: string, password: string, history: any) {
   return (dispatch: any) => {
     const options: RequestInit = {
       method: 'POST',
@@ -20,42 +11,34 @@ export function loginAlso(
     }
 
     fetch('/account/login', options)
-      .then(resp => {
-        if (resp.ok) {
-          return resp.json()
-        } else {
-          throw new Error()
-        }
-      })
-
-      .then(resp => {
-        dispatch({
-          type: 'LOG_IN',
-          user: resp
-        })
-
-        history.push('/')
-      })
-
-      .catch(error => {
-        console.log('there was an error', error)
-      })
+      .then(checkResp)
+      .then(resp => dispatch({ type: 'LOG_IN', user: resp }))
+      .then(() => history.push('/'))
+      .catch(error => console.log('there was an error', error))
   }
 }
 
 export function checkSession() {
   return (dispatch: Dispatch) => {
     return fetch('/account', { credentials: 'include' })
-      .then(resp => {
-        if (resp.ok) {
-          return resp.json()
-        } else {
-          throw new Error()
-        }
-      })
-
+      .then(checkResp)
       .then(resp => dispatch({ type: 'LOG_IN', user: resp }))
-
       .catch(error => console.log('did not log in'))
+  }
+}
+
+export function signup(state: any, history: any) {
+  return (dispatch: Dispatch) => {
+    const options: RequestInit = {
+      method: 'POST',
+      body: JSON.stringify(state),
+      credentials: 'include'
+    }
+
+    fetch('/account/signup', options)
+      .then(checkResp)
+      .then(resp => dispatch({ type: 'LOG_IN', user: resp }))
+      .then(() => history.push('/'))
+      .catch(error => console.log('there was an error', error))
   }
 }
