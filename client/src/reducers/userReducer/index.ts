e4034df96f596530b12f2a5e80a70ca7ce475f85
Alexander Urizar <urizaraj@gmail.com>
2018-07-23 17:26:45 -0400

    Include user collection reducer in user reducer

--- client/src/reducers/userReducer/index.ts
+++ client/src/reducers/userReducer/index.ts
@@ -1,8 +1,10 @@
 import form from 'reducers/userReducer/form'
 import { combineReducers } from 'redux'
+import { collection } from './collection'
 
 const userReducer = combineReducers({
-  form
+  form,
+  collection
 })
 
 export default userReducer
